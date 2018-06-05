fcf3c249c0692955b4b6bb78a5578b540f830034
nalkpas <lhefriel@gmail.com>
2018-06-05 09:34:59 -0700

    fixed some bugs in histogram code, reorganized it, included means

--- histogram_gen.py
+++ histogram_gen.py
@@ -9,25 +9,40 @@ scenarios = {'CityGrid_JeffersonCounty_CSV': ['InitialValues', 'DefensibleSpace'
 hist_names = ['damage_hist', 'lives_hist', 'length_hist']
 hist_titles = ['Building Damage', '# Deaths', 'Fire Lengths']
 hist_axis_labels = [('Building Damage', 'Proportion of Fires'), ('Lives Lost', 'Proportion of Fires'), ('Fire Length', 'Proportion of Fires')]
-num_episodes = 500
 
 n = len(hist_names)
 hists = []
+file_names = []
 for map_name in map_names:
 	for scenario in scenarios[map_name]:
+		hist_set = []
 		data_path = path = 'charts/histograms/' + scenario + '_' + map_name
 		for hist_name in hist_names:
 			with open(data_path + '/' + hist_name + '.txt', 'r') as file:
-				hists.append(np.array([float(line.strip()) for line in file]))
+				hist_set.append(np.array([float(line.strip()) for line in file]))
+		hists.append(hist_set)
 
-		fig, axs = plt.subplots(ncols = n)
+		file_names.append('charts/' + scenario + '-' + map_name + '_histogram.png')
 
-		for i in range(n):
-			axs[i] = sns.distplot(a=hists[i], ax=axs[i], hist=True, kde=False, norm_hist=False, axlabel=hist_axis_labels[i][0])
-			axs[i].set_title(hist_titles[i] + ' (' + str(num_episodes) + ' episodes)')
-			# ax[i].set(xlabel = hist_axis_labels[i][0], ylabel = hist_axis_labels[i][1])
-			axs[i].set_xlim(left=0)
-			axs[i].set_ylim(bottom=0,top=num_episodes * 3.5/5)
+hist_maxes = [0] * 3
+for scenario in hists:
+	for i in range(n):
+		if np.max(scenario[i]) > hist_maxes[i]:
+			hist_maxes[i] = np.max(scenario[i])
+hist_maxes = [hist_max * 2/3 for hist_max in hist_maxes]
 
-		fig.set_figwidth(20)
-		fig.savefig('charts/' + scenario + '-' + map_name + '_histogram.png')
+for scenario, file_name in zip(hists, file_names):
+	num_episodes = len(scenario[0])
+
+	fig, axs = plt.subplots(ncols = n)
+
+	for i in range(n):
+		axs[i] = sns.distplot(a=scenario[i], ax=axs[i], hist=True, kde=False, norm_hist=False, axlabel=hist_axis_labels[i][0])
+		axs[i].set_title(hist_titles[i] + ' (' + str(num_episodes) + ' episodes)')
+		# ax[i].set(xlabel = hist_axis_labels[i][0], ylabel = hist_axis_labels[i][1])
+		axs[i].text(hist_maxes[i]*0.5,num_episodes*0.6,'mean : ' + str(np.round(np.mean(scenario[i]),3)),fontsize=10)
+		axs[i].set_xlim(left=0,right=hist_maxes[i])
+		axs[i].set_ylim(bottom=0,top=num_episodes)
+
+	fig.set_figwidth(20)
+	fig.savefig(file_name)
