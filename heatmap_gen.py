395aac71d1ac986b23305de26cb30ac0ebc9dd85
nalkpas <lhefriel@gmail.com>
2018-06-05 18:27:03 -0700

    heatmap_gen counts files automatically now

--- heatmap_gen.py
+++ heatmap_gen.py
@@ -7,10 +7,10 @@ from PIL import Image
 
 path = 'charts/heatmap_1528244745/'
 image_path = 'charts/jefferson_county.JPG'
-fire_length = 13
+fire_length = len([name for name in os.listdir(path) if os.path.isfile(os.path.join(path, name)) and name.endswith('.txt')]) - 1
 
 map_image = Image.open(image_path)
-for i in range(fire_length + 1):
+for i in range(fire_length):
 	grid_state = np.loadtxt(path + str(i) + '.txt', delimiter=',')
 	fig, ax = plt.subplots(figsize=(5, 5))
 	sns.heatmap(grid_state, fmt="d", linewidths=.5, ax=ax, vmin=0, vmax=1, cmap='YlOrRd', alpha=0.4, cbar=False, zorder=2, xticklabels = False, yticklabels = False)
